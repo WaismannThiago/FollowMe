765d676acd8adbb8cd1c294e205bb41025b2aeb6
WaismannThiago <thiagowaismann@yahoo.com>
2025-06-03 23:37:03 -0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   apps: [
     {
-      name: "task-manager-trpc",
-      script: "npm",
-      args: "start",
+      name: 'followme-app',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      cwd: '/var/www/FollowMe-app',
       env: {
-        NODE_ENV: "production"
+        NODE_ENV: 'production',
+        PORT: 3000
       }
     }
   ]
-};
+}
