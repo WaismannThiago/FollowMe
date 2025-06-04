88ed2b765092011ed5e0c8dfe87c2eca4d14d4c6
WaismannThiago <thiagowaismann@yahoo.com>
2025-06-04 20:56:18 -0300

    Delete server directory

--- server/context.ts
+++
@@ -1,9 +0,0 @@
-// server/context.ts
-import { inferAsyncReturnType } from '@trpc/server';
-
-// Criação simples do contexto, sem auth
-export async function createContext() {
-  return {};
-}
-
-export type Context = inferAsyncReturnType<typeof createContext>;
--- server/routers/appRouter.ts
+++
@@ -1,6 +0,0 @@
-// server/routers/appRouter.ts
-import { router } from '@trpc/server';
-import { taskRouter } from './taskRouter';
-import type { Context } from '../context';
-
-export const appRouter = router<Context>().merge('task.', taskRouter);
