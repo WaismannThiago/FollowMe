da1fd093a8bf0658874ef0ca1110eabede4474ad
WaismannThiago <thiagowaismann@yahoo.com>
2025-06-04 20:46:05 -0300

    Update trpc.ts

--- utils/trpc.ts
+++ utils/trpc.ts
@@ -1,11 +1,4 @@
 import { createTRPCReact } from '@trpc/react-query';
 import type { AppRouter } from '../server/routers/appRouter';
-import { httpBatchLink } from '@trpc/client';
 
 export const trpc = createTRPCReact<AppRouter>();
-
-export const trpcClient = trpc.createClient({
-  links: [
-    httpBatchLink({ url: '/api/trpc' }),
-  ],
-});
