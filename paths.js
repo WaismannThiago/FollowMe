6516cd23d4248656eb304d41c0c8a4b8cd594900
WaismannThiago <thiagowaismann@yahoo.com>
2025-06-04 22:48:35 -0300

    Create paths.js

---
+++ paths.js
@@ -0,0 +1,7 @@
+const path = require('path')
+
+module.exports = {
+  '@': path.resolve(__dirname),
+  '@/utils': path.resolve(__dirname, 'utils'),
+  '@/server': path.resolve(__dirname, 'server')
+}
