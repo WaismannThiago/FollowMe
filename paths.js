b6a13d0fbcd259cbb1ff36fd8eca5962e149c484
WaismannThiago <thiagowaismann@yahoo.com>
2025-06-04 23:10:40 -0300

    Delete paths.js

--- paths.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  '@': path.resolve(__dirname),
-  '@/utils': path.resolve(__dirname, 'utils'),
-  '@/server': path.resolve(__dirname, 'server')
-}
