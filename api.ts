84c3ddd96f93591b9cd85291926fd1ad54e16d23
WaismannThiago <thiagowaismann@yahoo.com>
2025-06-04 23:26:33 -0300

    Delete api.ts

--- api.ts
+++
@@ -1,7 +0,0 @@
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-import { appRouter } from './router';
-
-export const GET = (req: Request) =>
-  fetchRequestHandler({ router: appRouter, createContext: () => ({}), req });
-
-export const POST = GET;
